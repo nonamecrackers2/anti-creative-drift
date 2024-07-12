0e652f3661e27fbb7fc458f44c2edafd1330f592
nonamecrackers2 <nonamecrackers2@outlook.com>
2024-07-12 13:44:06 -0600

    Moved client-side only stuff to the appropriate directories

--- src/client/java/dev/nonamecrackers2/anticreativedrift/AntiCreativeDrift.java
+++ src/client/java/dev/nonamecrackers2/anticreativedrift/AntiCreativeDrift.java
@@ -1,18 +1,13 @@
 package dev.nonamecrackers2.anticreativedrift;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import dev.nonamecrackers2.anticreativedrift.command.AntiCreativeDriftCommands;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
-public class AntiCreativeDrift implements ModInitializer
+public class AntiCreativeDrift implements ClientModInitializer
 {
-	public static final Logger LOGGER = LoggerFactory.getLogger("anticreativedrift");
-
 	@Override
-	public void onInitialize()
+	public void onInitializeClient()
 	{
 		 CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
 			 AntiCreativeDriftCommands.registerCommands(dispatcher);
